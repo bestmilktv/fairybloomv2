76023a6b08df3eeb79ef3de9c0ca13cf048df2b4
bestmilktv <bestmilktv@gmail.com>
2025-12-02 22:06:35 +0100

    gahwjkn

--- src/components/CategoryProductSection.tsx
+++ src/components/CategoryProductSection.tsx
@@ -30,7 +30,7 @@ const CategoryProductSection = ({ category, initialProducts }: CategoryProductSe
       {products.map((product, index) => (
         <div 
           key={product.id} 
-          className="fade-in-up overflow-visible p-4" 
+          className="fade-in-up overflow-visible px-4 pt-4 pb-6" 
           style={{ 
             animationDelay: `${0.4 + index * 0.1}s`
           }}
